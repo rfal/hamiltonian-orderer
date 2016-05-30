52a508d02d14675dc7b2dba6cee3272d81a1d49a
Renan Goupil <renan.goupil@gmail.com>
2016-05-30 12:27:58 +0200

    Implement Symbol class

--- orderer.py
+++ orderer.py
@@ -1,5 +1,75 @@
 class Symbol:
-    pass
+    '''
+    A symbol represents any scalar or operator in an expression. It has a symbol name, a behavior (subset with special properties to which it belongs) and a dagger attribute.
+
+    The list of all implemented behaviors is a class attributes, as well as the list of those behaviors which are hermitian (invariant under conjugation).
+    '''
+    _behaviors = ['zero', 'one', 'real', 'complex', 'annihilation'] # List of implemented behaviors for symbols (order matters for comparison!)
+    _hermitian_behaviors = ['zero', 'one', 'real'] # List of Hermitian behaviors for which the conj property is always False
+
+    def __init__(self, name, behavior, dag=False):
+        if behavior not in self._behaviors:
+            raise Exception('Behavior "' + behavior + '" not implemented.')
+
+        self.name = name
+        self.behavior = behavior
+
+        if behavior in self._hermitian_behaviors:
+            self.dag = False
+        else:
+            self.dag = dag
+
+    def __eq__(a, b):
+        return str(a) == str(b) and a.behavior == b.behavior and a.dag == b.dag # The str thing just enables us to compare zeros and ones, because a girl has no name
+
+    def __ne__(a,b):
+        return not (a == b)
+
+    def __lt__(a, b):
+        i_b_a = a._behaviors.index(a.behavior)
+        i_b_b = b._behaviors.index(b.behavior)
+
+        if i_b_a != i_b_b:
+            return i_b_a < i_b_b # We want the same order as the specified _behaviors list
+        else:
+            if a.behavior == "zero" or a.behavior == "one":
+                return False # All the zeros and the ones are the same
+            else:
+                if a.name != b.name:
+                    return a.name < b.name # Inside the same behavior we use the lexicographical orber
+                else:
+                    return False
+
+
+    def __le__(a, b):
+        return a < b or a == b or a.conj() == b or a.behavior == "zero" or a.behavior == "one" # Just because zero and one can have several names
+
+    def __gt__(a,b):
+        return b < a
+
+    def __ge__(a,b):
+        return b <= a
+
+    def __str__(self):
+        if self.behavior == 'zero':
+            return '0'
+        elif self.behavior == 'one':
+            return '1'
+        else:
+            res = self.name
+            if self.dag:
+                res = res + '*'
+
+            return res
+
+    def conj(self):
+        if self.name not in self._hermitian_behaviors:
+            return Symbol(self.name, self.behavior, not self.dag)
+        else:
+            return Symbol(self.name, self.behavior, False)
+
+ZERO = Symbol('0', 'zero')
+ONE = Symbol('1', 'one')
 
 class Term:
     pass
--- test_orderer.py
+++ test_orderer.py
@@ -2,12 +2,416 @@ import unittest
 from orderer import *
 
 class TestSymbol(unittest.TestCase):
-    pass
+    def test00100_instanciateZero_NameAndBehaviorOK(self):
+        # Arrange
+        zero = Symbol('0', 'zero')
+        
+        # Act
+        name = zero.name
+        behavior = zero.behavior
+
+        # Assert
+        self.assertEqual(name, '0')
+        self.assertEqual(behavior, 'zero')
+
+    def test00200_instanciateZero_ConjAlwaysFalse(self):
+        # Arrange
+        zero = Symbol('0', 'zero', True)
+
+        # Act
+        dag = zero.dag
+
+        # Assert
+        self.assertFalse(dag)
+
+    def test00300_instanciateOne_NameAndBehaviorOK(self):
+        # Arrange
+
+        # Act
+        one = Symbol('1', 'one')
+
+        name = one.name
+        behavior = one.behavior
+
+        # Assert
+        self.assertEqual(name, '1')
+        self.assertEqual(behavior, 'one')
+
+    def test00400_instanciateOne_ConjAlwaysFalse(self):
+        # Arrange
+        one = Symbol('1', 'one', True)
+
+        # Act
+        dag = one.dag
+
+        # Arrange
+        self.assertFalse(dag)
+
+    def test00500_instanciateComplexConjugate_ConjTrue(self):
+        # Arrange
+        z = Symbol('z', 'complex', True)
+
+        # Act
+        dag = z.dag
+
+        # Assert
+        self.assertTrue(dag)
+
+    def test00600_instanciateHermitianConjugate_ConjTrue(self):
+        # Arrange
+        a = Symbol('a', 'annihilation', True)
+
+        # Act
+        dag = a.dag
+
+        # Assert
+        self.assertTrue(dag)
+
+    def test00700_zeroAndOneInBehaviorList_OK(self):
+        # Arrange
+        behaviors = Symbol._behaviors
+
+        # Act
+
+        # Assert
+        self.assertIn('zero', behaviors)
+        self.assertIn('one', behaviors)
+
+    def test00800_instanciateNonExistingBehavior_error(self):
+        # Arrange
+
+        # Act
+
+        # Assert
+        self.assertRaises(Exception, lambda: Symbol('a', 'ThisBehaviorWillNeverExist'))
+
+    def test00900_instanciateTwoEqualSymbols_equal(self):
+        # Arrange
+        a1 = Symbol('a', 'annihilation')
+        a2 = Symbol('a', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertEqual(a1, a2)
+
+    def test01000_instancianteTwoDifferentSymbolNames_notEqual(self):
+        # Arrange
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertNotEqual(a, b)
+
+    def test01100_instancianteTwoDifferentSymbolBehaviors_notEqual(self):
+        # Arrange
+        a1 = Symbol('a', 'annihilation')
+        a2 = Symbol('a', 'complex')
+
+        # Act
+
+        # Assert
+        self.assertNotEqual(a1, a2)
+
+    def test01200_instancianteTwoDifferentSymbolDaggers_notEqual(self):
+        # Arrange
+        a = Symbol('a', 'annihilation')
+        a_dag = Symbol('a', 'annihilation', True)
+
+        # Act
+
+        # Assert
+        self.assertNotEqual(a, a_dag)
+
+    def test01210_classAttributeZero_OK(self):
+        # Arrange
+        zero = Symbol('0', 'zero')
+
+        # Act
+
+        # Assert
+        self.assertEqual(ZERO, zero)
+
+    def test01220_classAttributeOne_OK(self):
+        # Arrange
+        one = Symbol('1', 'one')
+
+        # Act
+
+        # Assert
+        self.assertEqual(ONE, one)
+
+    def test01300_conjugateZero_invariant(self):
+        # Arrange
+        zero = Symbol('0', 'zero')
+
+        # Act
+        zero_dag = zero.conj()
+
+        # Assert
+        self.assertEqual(zero_dag, zero)
+
+    def test01400_conjugateOne_invariant(self):
+        # Arrange
+        one = Symbol('1', 'one')
+
+        # Act
+        one_dag = one.conj()
+
+        # Assert
+        self.assertEqual(one_dag, one)
+
+    def test01500_conjugateReal_invariant(self):
+        # Arrange
+        x = Symbol('x', 'real')
+
+        # Act
+        x_dag = x.conj()
+
+        # Assert
+        self.assertEqual(x_dag, x)
+
+    def test01600_conjugateComplex_dagTrue(self):
+        # Arrange
+        z = Symbol('z', 'complex')
+
+        # Act
+        z_dag = z.conj()
+
+        # Assert
+        self.assertTrue(z_dag.dag)
+
+    def test01600_conjugateAnnihilation_dagTrue(self):
+        # Arrange
+        a = Symbol('a', 'annihilation')
+
+        # Act
+        a_dag = a.conj()
+
+        # Assert
+        self.assertTrue(a_dag.dag)
+
+    def test01700_doubleConjugatComplex_invariant(self):
+        # Arrange
+        z = Symbol('z', 'complex')
+
+        # Act
+        z_dag_dag = z.conj().conj()
+
+        # Assert
+        self.assertEqual(z, z_dag_dag)
+
+    def test01800_doubleConjugatAnnihilation_invariant(self):
+        # Arrange
+        a = Symbol('a', 'annihilation')
+
+        # Act
+        a_dag_dag = a.conj().conj()
+
+        # Assert
+        self.assertEqual(a, a_dag_dag)
+
+    def test01900_allHermitianBehaviorsInBehaviorList_OK(self):
+        # Arrange
+
+        # Act
+
+        # Assert
+        for b in Symbol._hermitian_behaviors:
+            self.assertIn(b, Symbol._behaviors)
+
+    def test02000_symbolComparisonLessThan_OK(self):
+        '''
+        The order of symbols is fully determined by this term:
+            0 . k^2 . n . xi*^4 . chi^2 . 1 . a*^2 . a . a* . b^2 . b*^7
+        '''
+        # Arrange
+        zero = Symbol('0', 'zero')
+        zero_2 = Symbol('00', 'zero')
+        one = Symbol('1', 'one')
+        one_2 = Symbol('11', 'one')
+        k = Symbol('k', 'real')
+        n = Symbol('n', 'real')
+        xi = Symbol('xi', 'complex')
+        zeta = Symbol('zeta', 'complex')
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertFalse(zero < zero_2)
+        self.assertFalse(one < one_2)
+        self.assertTrue(k < n)
+        self.assertTrue(xi < zeta)
+        self.assertTrue(a < b)
+
+        self.assertTrue(zero < k)
+        self.assertTrue(n < xi)
+        self.assertTrue(zeta < a)
+
+        self.assertFalse(xi.conj() < xi)
+        self.assertFalse(a.conj() < a)
+
+    def test02100_symbolComparisonLessThanEqual_OK(self):
+        '''
+        The order of symbols is fully determined by this term:
+            0 . k^2 . n . xi*^4 . chi^2 . 1 . a*^2 . a . a* . b^2 . b*^7
+        '''
+        # Arrange
+        zero = Symbol('0', 'zero')
+        zero_2 = Symbol('00', 'zero')
+        one = Symbol('1', 'one')
+        one_2 = Symbol('11', 'one')
+        k = Symbol('k', 'real')
+        n = Symbol('n', 'real')
+        xi = Symbol('xi', 'complex')
+        zeta = Symbol('zeta', 'complex')
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertTrue(zero <= zero_2)
+        self.assertTrue(one <= one_2)
+        self.assertTrue(k <= n)
+        self.assertTrue(xi <= zeta)
+        self.assertTrue(a <= b)
+
+        self.assertTrue(zero <= k)
+        self.assertTrue(n <= xi)
+        self.assertTrue(zeta <= a)
+
+        self.assertTrue(xi.conj() <= xi)
+        self.assertTrue(a.conj() <= a)
+
+    def test02200_symbolComparisonGreaterThan_OK(self):
+        '''
+        The order of symbols is fully determined by this term:
+            0 . k^2 . n . xi*^4 . chi^2 . 1 . a*^2 . a . a* . b^2 . b*^7
+        '''
+        # Arrange
+        zero = Symbol('0', 'zero')
+        zero_2 = Symbol('00', 'zero')
+        one = Symbol('1', 'one')
+        one_2 = Symbol('11', 'one')
+        k = Symbol('k', 'real')
+        n = Symbol('n', 'real')
+        xi = Symbol('xi', 'complex')
+        zeta = Symbol('zeta', 'complex')
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertFalse(zero > zero_2)
+        self.assertFalse(one > one_2)
+        self.assertTrue(n > k)
+        self.assertTrue(zeta > xi)
+        self.assertTrue(b > a)
+
+        self.assertTrue(k > zero)
+        self.assertTrue(xi > n)
+        self.assertTrue(a > zeta)
+
+        self.assertFalse(xi > xi.conj())
+        self.assertFalse(a > a.conj())
+
+    def test02200_symbolComparisonGreaterThanEqual_OK(self):
+        '''
+        The order of symbols is fully determined by this term:
+            0 . k^2 . n . xi*^4 . chi^2 . 1 . a*^2 . a . a* . b^2 . b*^7
+        '''
+        # Arrange
+        zero = Symbol('0', 'zero')
+        zero_2 = Symbol('00', 'zero')
+        one = Symbol('1', 'one')
+        one_2 = Symbol('11', 'one')
+        k = Symbol('k', 'real')
+        n = Symbol('n', 'real')
+        xi = Symbol('xi', 'complex')
+        zeta = Symbol('zeta', 'complex')
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertTrue(zero >= zero_2)
+        self.assertTrue(one >= one_2)
+        self.assertTrue(n >= k)
+        self.assertTrue(zeta >= xi)
+        self.assertTrue(b >= a)
+
+        self.assertTrue(k >= zero)
+        self.assertTrue(xi >= n)
+        self.assertTrue(a >= zeta)
+
+        self.assertTrue(xi >= xi.conj())
+        self.assertTrue(a >= a.conj())
+
+    def test02300_notDaggedSymbolStr_Name(self):
+        # Arrange
+        zero = Symbol('0', 'zero')
+        zero_2 = Symbol('00', 'zero')
+        one = Symbol('1', 'one')
+        one_2 = Symbol('11', 'one')
+        k = Symbol('k', 'real')
+        n = Symbol('n', 'real')
+        xi = Symbol('xi', 'complex')
+        zeta = Symbol('zeta', 'complex')
+        a = Symbol('a', 'annihilation')
+        b = Symbol('b', 'annihilation')
+
+        # Act
+
+        # Assert
+        self.assertEqual(str(zero), '0')
+        self.assertEqual(str(zero_2), '0')
+        self.assertEqual(str(one), '1')
+        self.assertEqual(str(one_2), '1')
+        self.assertEqual(str(k), 'k')
+        self.assertEqual(str(n), 'n')
+        self.assertEqual(str(xi), 'xi')
+        self.assertEqual(str(zeta), 'zeta')
+        self.assertEqual(str(a), 'a')
+        self.assertEqual(str(b), 'b')
+
+    def test02400_daggedSymbolStr_Name(self):
+        # Arrange
+        zero_dag = Symbol('0', 'zero', dag=True)
+        zero_2_dag = Symbol('00', 'zero', dag=True)
+        one_dag = Symbol('1', 'one', dag=True)
+        one_2_dag = Symbol('11', 'one', dag=True)
+        k_dag = Symbol('k', 'real', dag=True)
+        n_dag = Symbol('n', 'real', dag=True)
+        xi_dag = Symbol('xi', 'complex', dag=True)
+        zeta_dag = Symbol('zeta', 'complex', dag=True)
+        a_dag = Symbol('a', 'annihilation', dag=True)
+        b_dag = Symbol('b', 'annihilation', dag=True)
+
+        # Act
+
+        # Assert
+        self.assertEqual(str(zero_dag), '0')
+        self.assertEqual(str(zero_2_dag), '0')
+        self.assertEqual(str(one_dag), '1')
+        self.assertEqual(str(one_2_dag), '1')
+        self.assertEqual(str(k_dag), 'k')
+        self.assertEqual(str(n_dag), 'n')
+        self.assertEqual(str(xi_dag), 'xi*')
+        self.assertEqual(str(zeta_dag), 'zeta*')
+        self.assertEqual(str(a_dag), 'a*')
+        self.assertEqual(str(b_dag), 'b*')
 
 class TestTerm(unittest.TestCase):
     pass
 
-class TestExpression(unittest.TestCase):
+class TestExpression(unittest.TextCase):
     pass
 
 
